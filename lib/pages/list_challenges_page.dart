6ed88739cc6de86893ae9c320da88152a1f80baf
Sabagan Chandrakanthan <sabagan22@icloud.com>
2024-08-03 16:27:40 -0400

    Implemented Data Use

--- lib/pages/list_challenges_page.dart
+++ lib/pages/list_challenges_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ecoquest/model/challenge.dart';
+import 'package:ecoquest/utils/challenge_preferences.dart';
 
 class ListChallengesPage extends StatefulWidget {
   final bool isDarkMode;
@@ -10,100 +12,117 @@ class ListChallengesPage extends StatefulWidget {
 }
 
 class _ListChallengesPageState extends State<ListChallengesPage> {
-  final List<String> challenges = [
-    "Use a reusable straw",
-    "Recycle paper and plastic",
-    "Plant a tree",
-    "Use a reusable shopping bag",
-    "Reduce water usage",
-    "Turn off lights when not in use",
-    "Bike to work",
-    "Use public transportation",
-    "Compost food waste",
-    "Avoid single-use plastics"
-  ];
+  List<Challenge> _challenges = [];
 
-  final TextEditingController _controller = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _loadChallenges();
+  }
+
+  void _loadChallenges() {
+    setState(() {
+      _challenges = ChallengePreferences.getChallengeList();
+    });
+  }
 
   void _addCustomChallenge() {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        challenges.add(_controller.text);
-        _controller.clear();
-      });
-    }
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final titleController = TextEditingController();
+        final descriptionController = TextEditingController();
+
+        return AlertDialog(
+          title: Text('Add Custom Challenge'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(labelText: 'Title'),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text('Add'),
+              onPressed: () {
+                final newChallenge = Challenge(
+                  title: titleController.text,
+                  description: descriptionController.text,
+                  active: false,
+                );
+                ChallengePreferences.addChallenge(newChallenge);
+                _loadChallenges();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      color: widget.isDarkMode ? Colors.white : Colors.black,
-      fontSize: 18,
-      fontWeight: FontWeight.w500,
-    );
-
-    final inputTextStyle = TextStyle(
-      color: widget.isDarkMode ? Colors.white : Colors.black,
-      fontSize: 16,
-    );
-
     return Scaffold(
-      backgroundColor: widget.isDarkMode ? Colors.blue[400] : Colors.blue[100],
       appBar: AppBar(
-        title: const Text("List of Challenges"),
+        title: Text('List of Challenges'),
         backgroundColor:
-            widget.isDarkMode ? Colors.blue[100] : Colors.blue[400],
+            widget.isDarkMode ? Colors.blue[700] : Colors.blue[300],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: challenges.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    color: widget.isDarkMode ? Colors.black : Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(challenges[index], style: textStyle),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: "Add Custom Challenge",
-                labelStyle: inputTextStyle,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _challenges.length,
+          itemBuilder: (context, index) {
+            final challenge = _challenges[index];
+            return Card(
+              color: widget.isDarkMode ? Colors.blue[900] : Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              elevation: 4,
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(16.0),
+                title: Text(
+                  challenge.title,
+                  style: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                filled: true,
-                fillColor: widget.isDarkMode ? Colors.black : Colors.white,
-              ),
-              style: inputTextStyle,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                subtitle: Text(
+                  challenge.description,
+                  style: TextStyle(
+                    color: widget.isDarkMode ? Colors.white70 : Colors.black87,
+                    fontSize: 16,
+                  ),
                 ),
-                backgroundColor:
-                    widget.isDarkMode ? Colors.blue[100] : Colors.blue[400],
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                leading: Icon(
+                  Icons.eco,
+                  color: widget.isDarkMode ? Colors.greenAccent : Colors.green,
+                ),
               ),
-              onPressed: _addCustomChallenge,
-              child: const Text("Add Challenge",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ),
-          ],
+            );
+          },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addCustomChallenge,
+        child: Icon(Icons.add),
+        backgroundColor:
+            widget.isDarkMode ? Colors.blue[700] : Colors.blue[300],
       ),
     );
   }
